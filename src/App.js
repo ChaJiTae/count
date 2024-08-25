058b520de35625b4f490df43f6e509de72b7f39e
차지태 <wlxo402@naver.com>
2024-08-26 04:03:01 +0900

    #Design Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const count = useSelector((state) => state.count);
+  const currentCount = useState(0);
+  const computerRandomNumber = useState(0);
   const dispatch = useDispatch();
 
   const increase = (num) => {
@@ -15,12 +17,48 @@ function App() {
   };
 
   return (
-    <div>
-      <div>{count}</div>
-      <button onClick={() => increase(5)}>5 증가</button>
-      <button onClick={() => increase(1)}>1 증가</button>
-      <button onClick={() => decrease(1)}>1 감소</button>
-      <button onClick={() => decrease(5)}>5 증가</button>
+    <div className="backScreen">
+      <div className="mainScreen">
+        <div className="problemSet">
+          <div className="problemTitle">문제 : 최소 횟수 구하기</div>
+          <div className="problemExplanation">
+            무작위 숫자가 나타납니다. +5, +3, -3, -5를 사용하여 적은 횟수로
+            숫자가 되도록 만들어주세요
+          </div>
+        </div>
+        <div className="resultBoard">
+          <div className="computerRandomSet">
+            <div className="computerRandomTitle">컴퓨터 무작위 숫자</div>
+            <div className="computerRandomNumber">{computerRandomNumber}</div>
+          </div>
+          <div className="currentSet">
+            <div className="currentTitle">현재 값</div>
+            <div className="currentCount">{count}</div>
+          </div>
+          <div className="countSet">
+            <div className="countTitle">현재까지 횟수</div>
+            <div className="countResult">{currentCount}</div>
+          </div>
+        </div>
+        <div className="buttonSet">
+          <button className="increaseButton" onClick={() => increase(5)}>
+            5 증가
+          </button>
+          <button className="increaseButton" onClick={() => increase(3)}>
+            3 증가
+          </button>
+          <button className="decreaseButton" onClick={() => decrease(3)}>
+            3 감소
+          </button>
+          <button className="decreaseButton" onClick={() => decrease(5)}>
+            5 증가
+          </button>
+        </div>
+        <div className="boardAddDesign">
+          <div className="boardPencilRed"></div>
+        </div>
+      </div>
+      <div className="boardSupport"></div>
     </div>
   );
 }
